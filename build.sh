71360c2310770fb1410127c0d0c3513adffa1567
Nipun Sud <nipunsud@macbookpro.lan>
2025-01-07 18:30:09 -0500

    adding chrome

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Create local bin directory if it doesn't exist
+mkdir -p ~/bin
+
 # Install Chrome and Xvfb
 apt-get update && apt-get install -y \
     google-chrome-stable \
@@ -14,13 +17,16 @@ apt-get update && apt-get install -y \
 export DISPLAY=:99
 Xvfb :99 -screen 0 1024x768x16 &
 
-# Install ChromeDriver
+# Install ChromeDriver to user's bin directory
 CHROME_DRIVER_VERSION=`curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE`
 wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
-mv -f ~/chromedriver /usr/local/bin/chromedriver
-chmod 0755 /usr/local/bin/chromedriver
+chmod 0755 ~/chromedriver
+mv -f ~/chromedriver ~/bin/chromedriver
+
+# Add local bin to PATH
+export PATH=$PATH:~/bin
 
 # Install Python dependencies
 pip install -r requirements.txt

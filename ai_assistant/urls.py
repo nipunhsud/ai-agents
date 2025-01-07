70d96afd62557b2d31bbd4aecf4fbd57434c62e9
Nipun Sud <nipunsud@macbookpro.lan>
2025-01-07 01:12:03 -0500

    fixing gmail auth

--- ai_assistant/urls.py
+++ ai_assistant/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('agent/', views.AgentView.as_view(), name='agent'),
     path('react/', views.react_page, name='react_page'), 
-    path('test/', views.test, name='test'), 
+    path('query/', views.query_page, name='query_page'), 
     path("gift/",views.gift_prediction_view, name='gift'),
     path("slack/",test, name='slack'),
     path('writer/', views.technical_writer_view, name='technical_writer'),
@@ -24,6 +24,8 @@ urlpatterns = [
     path("email_assistant/",views.EmailAssistantView.as_view(), name='email_assistant'),
     path('analyst/', views.quant_analyst_page, name='quant_analyst_page'),
     path("stock_assistant/",views.StockAssistantView.as_view(), name='stock_assistant'),
+    path("email/",views.email_assistant_page, name='email_assistant_page'),
+    path('fetch_emails/', views.fetch_emails, name='fetch_emails'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 

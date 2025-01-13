5d375ed695a6bc40df89a64845d12c4255416c53
Nipun Sud <nipunsud@macbookpro.lan>
2025-01-12 22:05:26 -0500

    deleted chat files

--- chat/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Conversation, Message, Document
-
-admin.site.register(Conversation)
-admin.site.register(Message)
-admin.site.register(Document) 
